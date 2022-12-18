8a6481a43a1a3dfd8c91918e5f042b51ef497c32
Mujeeb <mujeebkhan1831@gmail.com>
2022-12-18 11:54:48 +0530

    dagger hlit Dependency injection

---
+++ app/src/main/java/com/example/movieinfo/hilt/HiltModule.kt
@@ -0,0 +1,11 @@
+package com.example.movieinfo.hilt
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object HiltModule {
+}
